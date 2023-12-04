017bef47dc03d5130a18c22ca85d8ad228e37325
dev91junior <devjk91@outlook.com>
2023-12-04 17:48:25 +0100

    get DOM elements

---
+++ FrontEnd/login.js
@@ -0,0 +1,9 @@
+
+/*** page login***/
+
+const email = document.getElementById("email")
+console.log(email)
+const password = document.getElementById('password')
+console.log(password)
+const form = document.querySelector('form')
+console.log(form)
