763229a26e7368cfe3ba1c4fa9a303e861b00650
dev91junior <devjk91@outlook.com>
2023-12-07 18:29:45 +0100

    add requestToken function + checkingLocalStorage function

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -8,64 +8,60 @@ console.log(password)
 const form = document.querySelector('form')
 
 // verifier si le token est stoker dans le local storage
-
-
-
-// si token store on redirige vers la page accueille 
-
-
-
+ function checkingLocalStorage (){
+    const checkToken = localStorage.getItem("token")
+    // si token store on redirige vers la page accueille 
+    if (checkToken) {
+        window.location.href = "../index.html"
+    }
+}
 
 
 // recuperer les email + password entrer par le user
 
+function requestToken () {
+    form.addEventListener('submit', async function(event){
+        event.preventDefault ();
+        // création objet contenant email + password
+        const userID = {
+            email: event.target.querySelector("[name=email]").value ,
+            password: event.target.querySelector("[name=password]").value,
+        };
+        console.log(userID)
+        // convertir la charge utile en Json
+        const chargeUtile = JSON.stringify(userID);
+        // appeler la fonction Fetch pour envoyer les données entré par le user
+            await fetch('http://localhost:5678/api/users/login', {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: chargeUtile,
+        })
 
-form.addEventListener('submit', function(event){
-    event.preventDefault ();
-    // création objet contenant email + password
-    const userID = {
-        email: event.target.querySelector("[name=email]").value ,
-        password: event.target.querySelector("[name=password]").value,
-    };
-    console.log(userID)
-    // convertir la charge utile en Json
-    const chargeUtile = JSON.stringify(userID);
-    // appeler la fonction Fetch pour envoyer les données entré par le user
-        fetch('http://localhost:5678/api/users/login', {
-        method: "POST",
-        headers: {"content-type": "application/json"},
-        body: chargeUtile,
-    })
+        // recuperer la réponse (l'id + token) de l'api 
+        .then((response) => {
+            if (!response.ok) {
+                return alert("le mot de passe ou l'email est incorrect")
 
-    // recuperer la réponse (l'id + token) de l'api 
-    .then((response) => {
-        if (!response.ok) {
-            return alert("le mot de passe ou l'email est incorrect")
+            } else {
+               return data = response.json()
+               // sauvegarder id + token dans le localstorage 
+                .then(data => {
+                    const userId = data.userId;
+                    window.localStorage.setItem("id", userId)
+                    const token = data.token;
+                    window.localStorage.setItem("token", token)
+                });  
+            };    
+        })
+        checkingLocalStorage()  
 
-        } else {
-           return data = response.json()
-            .then(data => {
-        
-                let id = data.userId;
-                console.log(id)
-                window.localStorage.setItem("id", data.userId)
-                let token = data.token;
-                console.log(token)
-                window.localStorage.setItem("token", token)
-            })
+    });
+}
+requestToken ()
 
-        }
-    })
-    .catch(error => console.log(error))
-        
-    
-    
 
-    // sauvegarder id + token dans le localstorage 
 
-});
-
-// qd on logout effacer le lacalstorage
+// qd on logout effacer le localstorage
 
 
 
