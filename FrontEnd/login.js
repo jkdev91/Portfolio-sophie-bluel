283e0577c3b8b7dd92cc38a705a3af97d6b3c5c6
dev91junior <devjk91@outlook.com>
2023-12-07 14:06:52 +0100

    move files to folder JS

--- FrontEnd/login.js
+++
@@ -1,9 +0,0 @@
-
-/*** page login***/
-
-const email = document.getElementById("email")
-console.log(email)
-const password = document.getElementById('password')
-console.log(password)
-const form = document.querySelector('form')
-console.log(form)
